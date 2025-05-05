98bffbefc6709ae5da9e07e2a20a9a23face1766
kont_sa <sanath.konthala@dlr.de>
2025-05-05 08:55:28 +0200

    updated optimizer to work with reference trajectory

--- include/planning/optinlc_trajectory_optimizer.hpp
+++ include/planning/optinlc_trajectory_optimizer.hpp
@@ -22,6 +22,7 @@
 
 #include "adore_math/angles.h"
 #include "adore_math/distance.h"
+#include "adore_math/PiecewisePolynomial.h"
 
 #include "OptiNLC_Data.h"
 #include "OptiNLC_OCP.h"
@@ -33,6 +34,13 @@ namespace adore
 {
 namespace planner
 {
+  struct reference_to_piecewise_polynomial
+  {
+    adore::math::PiecewisePolynomial::PiecewiseStruct x;
+    adore::math::PiecewisePolynomial::PiecewiseStruct y;
+    adore::math::PiecewisePolynomial::PiecewiseStruct v;
+    adore::math::PiecewisePolynomial::PiecewiseStruct heading;
+  };
 
 class OptiNLCTrajectoryOptimizer
 {
@@ -44,17 +52,19 @@ public:
     Y,
     PSI,
     V,
+    DELTA,
+    dDELTA,
+    S,
     L
   };
 
   enum CONTROLS
   {
-    DELTA,
-    ACC
+    ddDELTA
   };
 
-  static constexpr int    state_size       = 5;
-  static constexpr int    input_size       = 2;
+  static constexpr int    state_size       = 8;
+  static constexpr int    input_size       = 1;
   static constexpr int    control_points   = 30;
   static constexpr double sim_time         = 3.0; // Simulation time for the MPC
   static constexpr int    constraints_size = 0;
@@ -62,34 +72,63 @@ public:
 
 private:
 
-  double longitudinal_weight = 1.0;
-  double lateral_weight      = 1.0;
-  double velocity_weight     = 1.0;
-  double heading_weight      = 1.0;
-  double steering_weight     = 1.0;
-  double acceleration_weight = 1.0;
+  double longitudinal_weight = 0.0;
+  double lateral_weight      = 0.1;
+  double velocity_weight     = 0.25;
+  double heading_weight      = 0.25;
+  double steering_weight     = 0.0;
+  double acceleration_weight = 0.0;
+  double wheelbase                 = 2.69;
+  double max_forward_speed         = 13.6;
+  double max_reverse_speed         = -2.0;
+  double max_steering_velocity     = 0.25;
+  double max_steering_acceleration = 0.5;
+  double reference_velocity = 4.0;
 
   // Variables to store previous commands
   double last_steering_angle = 0.0;
   double last_acceleration   = 0.0;
   double bad_counter         = 0;
   int    counter             = 0;
+  double tau                 = 2.5;  // first order velocity profile
 
   // Variables for MPC solver configuration
   OptiNLC_Options options;
+  bool bad_condition       = false;
+  dynamics::Trajectory previous_trajectory;
+  double threshold_bad_output      = 20.0; // value of cost function above which is considered bad
+
+  struct Path
+  {
+    std::vector<double> s; // progress
+    std::vector<double> x;
+    std::vector<double> y;
+    std::vector<double> v;
+    std::vector<double> psi; // heading
+    std::vector<double> width;
+  } reference_to_follow;
+
+  // Variables to convert route to piecewise polynomial function
+  adore::math::PiecewisePolynomial                  pp;
+  adore::math::PiecewisePolynomial::PiecewiseStruct reference_trajectory_x;
+  adore::math::PiecewisePolynomial::PiecewiseStruct reference_trajectory_y;
+  adore::math::PiecewisePolynomial::PiecewiseStruct reference_trajectory_v;
+  adore::math::PiecewisePolynomial::PiecewiseStruct reference_trajectory_heading;
 
   // Helper function to define the dynamic model
-  void setup_dynamic_model( OptiNLC_OCP<double, OptiNLCTrajectoryOptimizer::input_size, OptiNLCTrajectoryOptimizer::state_size, 0,
-                                        OptiNLCTrajectoryOptimizer::control_points>& ocp,
-                            const dynamics::Trajectory&                              trajectory );
+  void setup_dynamic_model( OptiNLC_OCP<double, input_size, state_size, constraints_size, control_points>& ocp );
 
   // Helper function to define the objective function
-  void setup_objective_function( OptiNLC_OCP<double, OptiNLCTrajectoryOptimizer::input_size, OptiNLCTrajectoryOptimizer::state_size, 0,
-                                             OptiNLCTrajectoryOptimizer::control_points>& ocp );
+  void setup_objective_function( OptiNLC_OCP<double, input_size, state_size, constraints_size, control_points>& ocp );
 
   // Helper function to set constraints
-  void setup_constraints( OptiNLC_OCP<double, OptiNLCTrajectoryOptimizer::input_size, OptiNLCTrajectoryOptimizer::state_size, 0,
-                                      OptiNLCTrajectoryOptimizer::control_points>& ocp );
+  void setup_constraints( OptiNLC_OCP<double, input_size, state_size, constraints_size, control_points>& ocp );
+                                
+  // Helper function to set up route to follow to piecewise polynomial
+  void setup_reference_trajectory( reference_to_piecewise_polynomial& latest_reference_trajectory );
+
+  reference_to_piecewise_polynomial setup_optimizer_parameters_using_reference( const dynamics::Trajectory&          latest_reference_trajectory,
+                                                                                const dynamics::VehicleStateDynamic& current_state );
 
   // Helper function to set up the solver and solve the problem
   bool solve_mpc( OptiNLC_OCP<double, OptiNLCTrajectoryOptimizer::input_size, OptiNLCTrajectoryOptimizer::state_size, 0,
